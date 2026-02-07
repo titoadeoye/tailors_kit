e703b8b56eeb3accc5f56f78050f44b193aee9e8
titoadeoye <titoadeoye00@gmail.com>
2026-02-07 21:43:31 +0100

    refactor(config): make allowed hosts variable configurable in env

--- tailors_toolkit/settings.py
+++ tailors_toolkit/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = "django-insecure-z#=kvu4mj4i0qmlx-9_8z&u*ip16+js7-4*x+jw(5yflike!f$
 DEBUG = True
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-print('ALLOWED HOSTS', ALLOWED_HOSTS)
 
 # Application definition
 
